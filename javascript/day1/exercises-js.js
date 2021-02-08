b219246772050c06fa05abbe68c5cbea91b4ecbf
BlasToth <balazstothlaszlo+github@gmail.com>
2021-02-08 20:51:06 +0100

    add index file + style  to D12 + 1

--- javascript/day1/exercises-js.js
+++ javascript/day1/exercises-js.js
@@ -101,11 +101,11 @@ for (let i = 0; i < numeros.length; i++) {
     }
 }
 
+console.log(nuevoArray);
+
 console.log(linea);
 
-console.log("8. Escribir un script que simule el lanzamiento de dos dados. Hacer uso de la función Math.random() para obtener números aleatorios 
-//entre 1 y 6 para cada uno de los lanzamientos de los dados. Sumar el resultado de lanzar dos dados y anotar en un array el número de apariciones de dicha suma, 
-//repitiendo 36.000 veces esta operación.");
+console.log("8. Escribir un script que simule el lanzamiento de dos dados. Hacer uso de la función Math.random para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados. Sumar el resultado de lanzar dos dados y anotar en un array el número de apariciones de dicha suma, repitiendo 36.000 veces esta operación.");
 //Math.random(), for(), push()
 
 let arrayDeSumar = [];
