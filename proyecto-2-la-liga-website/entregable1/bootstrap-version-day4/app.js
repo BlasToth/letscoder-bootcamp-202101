7697b36037cd7463883af835531f4b7183d12d5e
BlasToth <balazstothlaszlo+github@gmail.com>
2021-02-11 15:53:39 +0100

    add json, styling, bootstrap container

---
+++ proyecto-2-la-liga-website/entregable1/bootstrap-version-day4/app.js
@@ -0,0 +1,70 @@
+const partidos = matchesData.matches;
+
+function getMatches(partidos) {
+  let tbody = document.querySelector(".table");
+
+  for (let i = 0; i < partidos.length; i++) {
+
+    let awayTeamID = partidos[i].awayTeam.id;
+    let homeTeamID = partidos[i].homeTeam.id;
+
+    let urlAwayTeam = "https://crests.football-data.org/" + awayTeamID + ".svg";
+    let urlHomeTeam = "https://crests.football-data.org/" + homeTeamID + ".svg";
+
+    let awayTeamName = partidos[i].awayTeam.name;
+    let homeTeamName = partidos[i].homeTeam.name;
+
+    let tr = document.createElement("tr"); // crear tr
+
+    let tdLocal = document.createElement("td"); // crear 3 td
+    tdLocal.innerText = partidos[i].homeTeam.name; // homeTeam - 1.
+
+    let tdResult = document.createElement("td"); // resultado - 2. | 0 - 0 |
+    if (partidos[i].score.fullTime.awayTeam === null) {
+      tdResult.innerText = partidos[i].status;
+      tdResult.classList.add("tdresult");
+      tdResult.classList.add("red");
+    } else {
+       tdResult.classList.add("tdresult");
+        tdResult.style.textAlign = "center";
+      // tdResult.setAttribute("class", "tdresult");
+
+      // tdResult.style.textAlign = "center";
+
+      tdResult.innerHTML = `
+      <span><img src="${urlHomeTeam}" alt="${homeTeamName}" width="40px" title="${homeTeamName}"></span>
+      <span>${partidos[i].score.fullTime.homeTeam}</span>
+      <span>-<span>
+      <span>${partidos[i].score.fullTime.awayTeam}</span>
+      <span><img src="${urlAwayTeam}" alt="${awayTeamName}" width="40px" title="${awayTeamName}"></span>
+      `
+    }
+
+    let tdVisitante = document.createElement("td"); // awayTeam -3.
+    tdVisitante.innerText = partidos[i].awayTeam.name;
+
+    tr.append(tdLocal); // append td to tr
+    tr.append(tdResult);
+    tr.append(tdVisitante);
+    tbody.append(tr); // append  tr to tbody
+
+    // search
+
+    // const searchInput = document.getElementById("search");
+    // const rows = document.querySelectorAll("tbody tr");
+
+    //   searchInput.addEventListener('keyup', function(event) {
+    //     const q = event.target.value.toLowerCase();
+    //     rows.forEach(row => {
+    //       row.querySelector('td').textContent.toLowerCase().startsWith(q) ? (row.style.display = "table-row") : (row.style.display = "none");
+    //   })
+    // });
+
+    // end search
+
+  }
+}
+
+getMatches(partidos);
+
+
