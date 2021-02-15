5ef661a06a9a8528e029a103d9981770528a148b
BlasToth <balazstothlaszlo+github@gmail.com>
2021-02-15 11:08:00 +0100

    add basic files to start counting statistics

---
+++ proyecto-2-la-liga-website/entregable1/bootstrap-version-day4/top5.js
@@ -0,0 +1,34 @@
+const partidos = matchesData.matches;
+
+// 0. Crear función que va calcular las estadísticas, recibiendo como param el array de partidos
+
+function countStats(partidos) {
+    let statistics = [];
+
+    for (let i = 0; i < partidos.length; i++) {
+        if ()
+    }
+}
+
+countStats(partidos);
+
+// 1. Crear array vacía (será array de objetos)
+
+// 2. Iterar por todos los partidos
+
+// 3. Condición: si el partido no está acabado, no seguir y mirar siguiente partido
+
+// 4. Buscar en la array estadisticas el objeto con el mismo id que el homeTeam del partido
+
+// 5. Si el objeto buscado no existe, crearlo con estos keys: id, name, goals, matches.
+// Rellenar cada key con el valor correspondiente
+
+// 6. Si existe, actualizar los goles y los partidos
+
+// 7. Hacer exactamente lo mismo a partir del punto 4, pero con awayTeam
+
+// 8. Una vez fuera del loop de partidos, iterar por el array estadisticas
+
+// 9. Añadir la key avg a cada objeto, con el valor goals/matches
+
+// 10. Hacer console.log() para ver que todo está correcto.
