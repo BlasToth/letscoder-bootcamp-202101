eb768e7ee5025c5c0dd4beb88261030a95b8e5cd
BlasToth <balazstothlaszlo+github@gmail.com>
2021-02-17 18:32:02 +0100

    return to last working version

--- proyecto-2-la-liga-website/entregable1/bootstrap-version-day4/app.js
+++ proyecto-2-la-liga-website/entregable1/bootstrap-version-day4/app.js
@@ -1,167 +1,65 @@
-// radio buttons
-{/* <input type="radio" name="filter" value="*" checked="checked"  /> todos */}
-const radioButtons = document.getElementsByName("filter");
-
-
-// end radio buttons
 const partidos = matchesData.matches;
-const ganados = [];
-
 
 function getMatches(partidos) {
   let tbody = document.querySelector(".partidos-body");
-  
-  
+
   for (let i = 0; i < partidos.length; i++) {
 
-      let awayTeamID = partidos[i].awayTeam.id;
-      let homeTeamID = partidos[i].homeTeam.id;
+    let awayTeamID = partidos[i].awayTeam.id;
+    let homeTeamID = partidos[i].homeTeam.id;
 
-      let urlAwayTeam = "https://crests.football-data.org/" + awayTeamID + ".svg";
-      let urlHomeTeam = "https://crests.football-data.org/" + homeTeamID + ".svg";
+    let urlAwayTeam = "https://crests.football-data.org/" + awayTeamID + ".svg";
+    let urlHomeTeam = "https://crests.football-data.org/" + homeTeamID + ".svg";
 
-      let awayTeamName = partidos[i].awayTeam.name;
-      let homeTeamName = partidos[i].homeTeam.name;
+    let awayTeamName = partidos[i].awayTeam.name;
+    let homeTeamName = partidos[i].homeTeam.name;
 
-      let tr = document.createElement("tr"); // crear tr
+    let tr = document.createElement("tr"); // crear tr
 
-      let tdLocal = document.createElement("td"); // crear 3 td
-      tdLocal.innerText = partidos[i].homeTeam.name; // homeTeam - 1.
+    let tdLocal = document.createElement("td"); // crear 3 td
+    tdLocal.innerText = partidos[i].homeTeam.name; // homeTeam - 1.
 
-      let tdResult = document.createElement("td"); // resultado - 2. | 0 - 0 |
+    let tdResult = document.createElement("td"); // resultado - 2. | 0 - 0 |
+    if (partidos[i].score.fullTime.awayTeam === null) {
+      tdResult.innerText = partidos[i].status;
+      tdResult.classList.add("scheduled");
+    } else {
+       tdResult.style.textAlign = "center";
+      tdResult.innerHTML = `
+      <span><img src="${urlHomeTeam}" alt="${homeTeamName}" width="40px" title="${homeTeamName}"></span>
+      <span>${partidos[i].score.fullTime.homeTeam}</span>
+      <span>-<span>
+      <span>${partidos[i].score.fullTime.awayTeam}</span>
+      <span><img src="${urlAwayTeam}" alt="${awayTeamName}" width="40px" title="${awayTeamName}"></span>
+      `
+    }
 
-      // get data
+    let tdVisitante = document.createElement("td"); // awayTeam -3.
+    tdVisitante.innerText = partidos[i].awayTeam.name;
+    tdVisitante.style.textAlign = "right";
 
-      // if todos
-
-    // ganados - HOME_TEAM
-    for (let j = 0; j < radioButtons.length; j++) {
-
-            radioButtons[j].addEventListener('click', () => {
-              
-              if (radioButtons[j].checked) {
-                if (partidos[i].score.winner === "HOME_TEAM") {
-                  ganados.push(partidos[i]);
-                  
-                  for (let k = 0; k < ganados.length; k++) {
-                    if (partidos[i].score.fullTime.awayTeam === null) {
-                      tdResult.innerText = partidos[i].status;
-                      tdResult.classList.add("scheduled");
-                    } else {
-  
-                      let tdLocal = document.createElement("td"); // crear 3 td
-                      tdLocal.innerText = partidos[i].homeTeam.name; // homeTeam - 1.
-  
-                      let tdResult = document.createElement("td"); // resultado - 2. | 0 - 0 |
-                      tdResult.style.textAlign = "center";
-                      tdResult.innerHTML = `
-                      <span><img src="${urlHomeTeam}" alt="${homeTeamName}" width="40px" title="${homeTeamName}"></span>
-                      <span>${partidos[i].score.fullTime.homeTeam}</span>
-                      <span>-<span>
-                      <span>${partidos[i].score.fullTime.awayTeam}</span>
-                      <span><img src="${urlAwayTeam}" alt="${awayTeamName}" width="40px" title="${awayTeamName}"></span>
-                      `
-                    }
-            
-                    let tdVisitante = document.createElement("td"); // awayTeam -3.
-                    tdVisitante.innerText = partidos[i].awayTeam.name;
-                    tdVisitante.style.textAlign = "right";
-            
-                    tr.append(tdLocal); // append td to tr
-                    tr.append(tdResult);
-                    tr.append(tdVisitante);
-                    tbody.append(tr); // append  tr to tbody
-                  }
-
-
-                  }
-                
-                }
-          
-
-        });
-  }
-
-    
-      // if empatados - DRAW
-          if (partidos[i].score.winner === "DRAW") {
-            ganados.push(partidos[i]);
-          
-            if (partidos[i].score.fullTime.awayTeam === null) {
-              tdResult.innerText = partidos[i].status;
-              tdResult.classList.add("scheduled");
-            } else {
-              tdResult.style.textAlign = "center";
-              tdResult.innerHTML = `
-              <span><img src="${urlHomeTeam}" alt="${homeTeamName}" width="40px" title="${homeTeamName}"></span>
-              <span>${partidos[i].score.fullTime.homeTeam}</span>
-              <span>-<span>
-              <span>${partidos[i].score.fullTime.awayTeam}</span>
-              <span><img src="${urlAwayTeam}" alt="${awayTeamName}" width="40px" title="${awayTeamName}"></span>
-              `
-            }
-    
-            let tdVisitante = document.createElement("td"); // awayTeam -3.
-            tdVisitante.innerText = partidos[i].awayTeam.name;
-            tdVisitante.style.textAlign = "right";
-    
-            tr.append(tdLocal); // append td to tr
-            tr.append(tdResult);
-            tr.append(tdVisitante);
-            tbody.append(tr); // append  tr to tbody
-          }
-
-          // if perdidos - AWAY_TEAM
-                if (partidos[i].score.winner === "AWAY_TEAM") {
-                  ganados.push(partidos[i]);
-                
-                  if (partidos[i].score.fullTime.awayTeam === null) {
-                    tdResult.innerText = partidos[i].status;
-                    tdResult.classList.add("scheduled");
-                  } else {
-                    tdResult.style.textAlign = "center";
-                    tdResult.innerHTML = `
-                    <span><img src="${urlHomeTeam}" alt="${homeTeamName}" width="40px" title="${homeTeamName}"></span>
-                    <span>${partidos[i].score.fullTime.homeTeam}</span>
-                    <span>-<span>
-                    <span>${partidos[i].score.fullTime.awayTeam}</span>
-                    <span><img src="${urlAwayTeam}" alt="${awayTeamName}" width="40px" title="${awayTeamName}"></span>
-                    `
-                  }
-          
-                  let tdVisitante = document.createElement("td"); // awayTeam -3.
-                  tdVisitante.innerText = partidos[i].awayTeam.name;
-                  tdVisitante.style.textAlign = "right";
-          
-                  tr.append(tdLocal); // append td to tr
-                  tr.append(tdResult);
-                  tr.append(tdVisitante);
-                  tbody.append(tr); // append  tr to tbody
-                }
-
-    // end get data
+    tr.append(tdLocal); // append td to tr
+    tr.append(tdResult);
+    tr.append(tdVisitante);
+    tbody.append(tr); // append  tr to tbody
 
     // TO DO
     // filter by name, radiobuttons
     // search
 
-      const searchInput = document.getElementById("search");
-      const rows = document.querySelectorAll("tbody tr");
+    const searchInput = document.getElementById("search");
+    const rows = document.querySelectorAll("tbody tr");
 
-        searchInput.addEventListener('keyup', function(event) {
-          const q = event.target.value.toLowerCase();
-          rows.forEach(row => {
-            row.querySelector('td').textContent.toLowerCase().startsWith(q) ? (row.style.display = "") : (row.style.display = "none");
-        })
-      });
+      searchInput.addEventListener('keyup', function(event) {
+        const q = event.target.value.toLowerCase();
+        rows.forEach(row => {
+          row.querySelector('td').textContent.toLowerCase().startsWith(q) ? (row.style.display = "table-row") : (row.style.display = "none");
+      })
+    });
 
-      // end search
-      
-      
-}
+    // end search
+
+  }
 }
 
 getMatches(partidos);
-
-
-
