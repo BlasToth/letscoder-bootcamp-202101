d39475a87e4b87ba187934fc1dc331ac68b2f64a
BlasToth <balazstothlaszlo+github@gmail.com>
2021-03-03 09:37:08 +0100

    refactor

--- proyecto-3-own-project/server.js
+++ proyecto-3-own-project/server.js
@@ -2,10 +2,24 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const port = 4000;
 
 app.use(bodyParser.urlencoded( {extended: true} ));
 
-mongoose.connect("mongodb+srv://bago:BfLExgnD2F19btN5@bcapp.eoazq.mongodb.net/verbs", { useNewUrlParser: true }, { useUnifiedTopology: true } );
+mongoose
+.connect(
+    "mongodb+srv://bago:BfLExgnD2F19btN5@bcapp.eoazq.mongodb.net/verbs",
+    { useNewUrlParser: true },
+    { useUnifiedTopology: true } 
+)
+.then(result => {
+    app.listen(port, () => {
+        console.log(`Server is running on ${port}`)
+    });
+})
+.catch(err => {
+    console.log(`THERE WAS AN ERROR: ${err.message}`);
+});
 
 // create a data schema
 const myDataSchema = {
@@ -27,6 +41,6 @@ app.post("/", (req, res) => {
     res.redirect('/');
 })
 
-app.listen(4000, () => {
-    console.log("server is running on 4000");
-})
+// app.listen(4000, () => {
+//     console.log("server is running on 4000");
+// })
