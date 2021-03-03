73e934dd26e30a2ff9f9e8829e31a03906f0e277
BlasToth <balazstothlaszlo+github@gmail.com>
2021-03-03 21:04:26 +0100

    add login-model

--- proyecto-3-own-project/server.js
+++ proyecto-3-own-project/server.js
@@ -23,72 +23,9 @@ mongoose
     console.log(`THERE WAS AN ERROR: ${err.message}`);
 });
 
-// // create a data schema
-// const Schema = mongoose.Schema;
-
-// const verbsSchema = new Schema({
-//     sourceName: String,
-//     v1: String,
-//     v2: String,
-//     v3: String,
-//     wrongV1: String,
-//     wrongV2: String,
-//     wrongV3: String
-// });
-// // We'll work with this model in our code
-// // Mongoose makes the name into lower case and makes it plural
-// const Verb = mongoose.model('Verb', verbsSchema);
-
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/admin/admin.html");
 })
 
 app.use("/verbs", verbs);
 
-
-// app.post("/", (req, res) => {
-//     console.log(req.body)
-//     const verb = new Verb({
-//         sourceName: req.body.sourceName,
-//         v1: req.body.v1,
-//         v2: req.body.v2,
-//         v3: req.body.v3,
-//         wrongV1: req.body.wrongv1,
-//         wrongV2: req.body.wrongv2,
-//         wrongV3: req.body.wrongv3
-//     })
-//     console.log(verb)
-//     verb
-//     .save()
-//     .then(result => {
-//         console.log('Created Verb');
-//     })
-//     .catch(err => {
-//         console.log(err);
-//     })
-//     res.redirect('/');
-// })
-
-// app.get("/verbs", (req, res) => {
-//     Verb.find({}, (err, verbs) => {
-//         if (err) {
-//             res.status(404).send(err.response.data);
-//         }
-//         res.json(verbs)
-//     })
-// })
-
-
-
-
-// Verb.find({v1: 'bleed'}, (error, data) => {
-//     if (error) {
-//         console.log(error);
-//     } else {
-//         console.log(data);
-//     }
-// })
-
-// app.listen(4000, () => {
-    //     console.log("server is running on 4000");
-    // })
