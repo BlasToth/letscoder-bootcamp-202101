d3415524f988b5f279cb52f1fa18e73229ef7546
BlasToth <balazstothlaszlo+github@gmail.com>
2021-03-06 22:37:32 +0100

    refactor audio-handler.js

---
+++ proyecto-3-own-project/public/js/audio-handler.js
@@ -0,0 +1,12 @@
+        // let a = 0;
+        document.getElementById("figcaption").addEventListener("click",function(){
+            // if(a==0) {
+                document.getElementById("audio").play();
+                // a++;
+                // document.getElementById("figcaption").style.opacity="1";
+            // }else{
+            //     document.getElementById("audio").pause();
+            //     a--;
+                // document.getElementById("figcaption").style.opacity=".5";
+            // }
+        });
