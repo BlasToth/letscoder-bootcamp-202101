c3f294587aeecf29378f5efe0973cf4eac76c624
BlasToth <balazstothlaszlo+github@gmail.com>
2021-03-23 21:30:34 +0100

    every user has its own array of verbs

--- proyecto-3-own-project/router/verb-router.js
+++ proyecto-3-own-project/router/verb-router.js
@@ -94,12 +94,52 @@ verbRouter.route("/answers").get(authenticateToken, (req, res) => {
           );
           // console.log(difference);
           // Find the verbs with these new IDs
-          Verb.find({_id: difference}, (err, verbs) => {
+          Verb.find({ _id: difference }, (err, verbs) => {
             if (err) {
               res.status(404).send(err.response.data);
             } else {
-              const filteredVerbs = verbs
-              console.log(`new verbs: ${filteredVerbs}` )
+              const filteredVerbs = verbs;
+              // console.log(`new verbs: ${filteredVerbs}` )
+
+              const randomVerb =
+                filteredVerbs[Math.floor(Math.random() * filteredVerbs.length)];
+              const {
+                sourceName,
+                v1,
+                v2,
+                v3,
+                wrongV1,
+                wrongV2,
+                wrongV3,
+                _id,
+                gifUrl,
+                audioUrl,
+              } = randomVerb;
+
+              // send random v form
+              const vForms = [v1, v2, v3];
+              randomVFormNum = Math.floor(Math.random() * 3);
+              // subtract one from the forms
+              let showVForm = [];
+              if (randomVFormNum === 0) {
+                let answersForCase0 = wrongV1.split(", ");
+                answersForCase0.push(v1);
+                shuffle(answersForCase0);
+                showVForm.push("case 0", v2, v3, _id, answersForCase0);
+              } else if (randomVFormNum === 1) {
+                let answersForCase1 = wrongV2.split(", ");
+                answersForCase1.push(v2);
+                shuffle(answersForCase1);
+                showVForm.push(v1, "case 1", v3, _id, answersForCase1);
+              } else if (randomVFormNum === 2) {
+                let answersForCase2 = wrongV3.split(", ");
+                answersForCase2.push(v3);
+                shuffle(answersForCase2);
+                showVForm.push(v1, v2, "case 2", _id, answersForCase2);
+              }
+              // send random v form end
+              showVForm.push(gifUrl, audioUrl, sourceName);
+              res.send(showVForm);
             }
           });
           // Find ends here
@@ -107,44 +147,6 @@ verbRouter.route("/answers").get(authenticateToken, (req, res) => {
       });
 
       // filter ends
-      const randomVerb = verbs[Math.floor(Math.random() * verbs.length)];
-      const {
-        sourceName,
-        v1,
-        v2,
-        v3,
-        wrongV1,
-        wrongV2,
-        wrongV3,
-        _id,
-        gifUrl,
-        audioUrl,
-      } = randomVerb;
-
-      // send random v form
-      const vForms = [v1, v2, v3];
-      randomVFormNum = Math.floor(Math.random() * 3);
-      // subtract one from the forms
-      let showVForm = [];
-      if (randomVFormNum === 0) {
-        let answersForCase0 = wrongV1.split(", ");
-        answersForCase0.push(v1);
-        shuffle(answersForCase0);
-        showVForm.push("case 0", v2, v3, _id, answersForCase0);
-      } else if (randomVFormNum === 1) {
-        let answersForCase1 = wrongV2.split(", ");
-        answersForCase1.push(v2);
-        shuffle(answersForCase1);
-        showVForm.push(v1, "case 1", v3, _id, answersForCase1);
-      } else if (randomVFormNum === 2) {
-        let answersForCase2 = wrongV3.split(", ");
-        answersForCase2.push(v3);
-        shuffle(answersForCase2);
-        showVForm.push(v1, v2, "case 2", _id, answersForCase2);
-      }
-      // send random v form end
-      showVForm.push(gifUrl, audioUrl, sourceName);
-      res.send(showVForm);
     }
   });
 });
