42ba51a610eca22abcdee8baa49e09401c7ec503
BlasToth <balazstothlaszlo+github@gmail.com>
2021-03-25 20:22:20 +0100

    import Links and build links in Nav.js

--- proyecto-4-frontend/my-react-front/my-react-front/src/components/Nav.js
+++ proyecto-4-frontend/my-react-front/my-react-front/src/components/Nav.js
@@ -1,23 +1,36 @@
 // import './Nav.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
 
 function Nav() {
-    return (
-        <nav>
-            <h3>Logo</h3>
-            <ul>
-                <li>Home</li>
-                <li>Admin</li>
-                <li>All verbs</li>
-                <li>Random verb</li>
-                <li>Quiz</li>
-                <li>Known verbs</li>
-                <li>Hall of fame</li>
-            </ul>
-        </nav>
-    );
-  }
-  
-  export default Nav;
+  return (
+    <nav>
+      <h3>Logo</h3>
+      <ul>
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+        <Link to="/admin">
+          <li>Admin</li>
+        </Link>
+        <Link to="/allverbs">
+          <li>All verbs</li>
+        </Link>
+        <Link to="/randomverb">
+          <li>Random Verb</li>
+        </Link>
+        <Link to="/quiz">
+          <li>Quiz</li>
+        </Link>
+        <Link to="/knownverbs">
+          <li>Known verbs</li>
+        </Link>
+        <Link to="/halloffame">
+          <li>Hall of fame</li>
+        </Link>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
