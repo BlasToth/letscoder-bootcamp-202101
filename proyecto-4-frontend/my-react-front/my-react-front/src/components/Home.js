291819484fe914eb03112bf74a61b824d3f06ebd
BlasToth <balazstothlaszlo+github@gmail.com>
2021-03-26 18:08:30 +0100

    play around with forms in React

--- proyecto-4-frontend/my-react-front/my-react-front/src/components/Home.js
+++ proyecto-4-frontend/my-react-front/my-react-front/src/components/Home.js
@@ -1,36 +1,117 @@
 // import './Nav.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useReducer, useState } from "react";
 
-
+const formReducer = (state, event) => {
+    if (event.reset) {
+        return {
+            email: '',
+            password: '',
+            apple: '',
+            count: 0,
+            'gift-wrap': false
+        }
+    }
+    return {
+        ...state,
+        [event.name]: event.value
+    }
+}
 
 function Home() {
-    return (
-        <>
-        <h1>Welcome!</h1>
-        <h2>Please log in!</h2>
-        <div class="login content">
+    const [formData, setFormData] = useReducer(formReducer, {});
+    const [submitting, setSubmitting] = useState(false);
+    const handleSubmit = event => {
+        event.preventDefault();
+        setSubmitting(true);
 
-        <form action="/login" method="POST">
-            <p>Email: <input type="email" name="email" required/></p>
-            <p>Password: <input type="password" name="password" required/></p>
-            <input type="submit" name="sub" value="Submit" />
+        setTimeout(() => {
+            setSubmitting(false);
+        setFormData({
+            reset: true
+        })
+          }, 3000);
+    }
+
+    const handleChange = event => {
+        const isCheckbox = event.target.type === 'checkbox';
+        setFormData({
+            name: event.target.name,
+            value: isCheckbox ? event.target.checked : event.target.value
+        });
+    }
+
+
+  return (
+    <>
+      <h1>Welcome!</h1>
+      <h2>Please log in!</h2>
+      <div className="login content">
+      {submitting &&
+       <div>
+         You are submitting the following:
+         <ul>
+           {Object.entries(formData).map(([name, value]) => (
+             <li key={name}><strong>{name}</strong>:{value.toString()}</li>
+           ))}
+         </ul>
+       </div>
+      }
+        <form onSubmit={handleSubmit}>
+          <fieldset>
+            <label>
+              <p>Email</p>
+              <input name="email" onChange={handleChange} value={formData.email || ''}/>
+            </label>
+          </fieldset>
+          <fieldset>
+            <label>
+              <p>Password</p>
+              <input name="password" onChange={handleChange} value={formData.password || ''}/>
+            </label>
+          </fieldset>
+          <fieldset>
+         <label>
+           <p>Apples</p>
+           <select name="apple" onChange={handleChange} value={formData.apple || ''}>
+               <option value="">--Please choose an option--</option>
+               <option value="fuji">Fuji</option>
+               <option value="jonathan">Jonathan</option>
+               <option value="honey-crisp">Honey Crisp</option>
+           </select>
+         </label>
+         <label>
+           <p>Count</p>
+           <input type="number" name="count" onChange={handleChange} step="1" value={formData.count || ''}/>
+         </label>
+         <label>
+           <p>Gift Wrap</p>
+           <input type="checkbox" name="gift-wrap" onChange={handleChange} checked={formData['gift-wrap'] || false} disabled={formData.apple !== 'fuji'}/>
+         </label>
+       </fieldset>
+          <button type="submit">LOG IN</button>
         </form>
-        </div>
+      </div>
 
-        <hr></hr>
+      <hr></hr>
 
-        <h2>Sign up if you are new here: </h2>
-        <div class="signup content">
-
+      <h2>Sign up if you are new here: </h2>
+      <div className="signup content">
         <form action="/signup" method="POST">
-            <p>Email: <input type="email" name="email" required/></p>
-            <p>Password: <input type="password" name="password" required/></p>
-            <p>Nickname: <input type="text" name="nickname" required/></p>
-            <input type="submit" name="sub" value="Submit" />
+          <p>
+            Email: <input type="email" name="email" required />
+          </p>
+          <p>
+            Password: <input type="password" name="password" required />
+          </p>
+          <p>
+            Nickname: <input type="text" name="nickname" required />
+          </p>
+          <input type="submit" name="sub" value="Submit" />
         </form>
-    </div>
-        </>
-    );
-  }
-  
-  export default Home;
+      </div>
+    </>
+  );
+}
+
+export default Home;
