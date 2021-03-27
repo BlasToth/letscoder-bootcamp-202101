7c170b9ef00c21923c60cddccc3424d168ff9d88
BlasToth <balazstothlaszlo+github@gmail.com>
2021-03-27 10:31:34 +0100

    build custom hook

--- login-react/login-react/src/App.js
+++ login-react/login-react/src/App.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Login from "./components/Login/Login";
 import Preferences from "./components/Preferences/Preferences";
+import useToken from './useToken';
 
 
 function App() {
-  const [token, setToken] = useState();
+  const { token, setToken } = useToken();
 
   if (!token) {
     return <Login setToken={setToken}/>
