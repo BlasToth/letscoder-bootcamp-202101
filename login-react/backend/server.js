d66536bb1e2a27a86140c07d6bc17b358924be8b
BlasToth <balazstothlaszlo+github@gmail.com>
2021-03-27 09:50:01 +0100

    build a login route with a token

--- login-react/backend/server.js
+++ login-react/backend/server.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
+
+app.use('/login', (req, res) => {
+    res.send({
+        token: 'test123'
+    });
+});
 
 app.listen(4000, () => console.log('App listening on port 4000'))
