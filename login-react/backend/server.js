2cdc26516bda9d6bb206bf569f59e0f55181b211
BlasToth <balazstothlaszlo+github@gmail.com>
2021-03-27 17:53:50 +0100

    build secret token key and add dotenv

--- login-react/backend/server.js
+++ login-react/backend/server.js
@@ -1,13 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+const jwt = require('jsonwebtoken');
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+
 
 app.use('/login', (req, res) => {
-    res.send({
-        token: 'test123'
+    const username = "Pepito Grillo"
+    const user = { name: username };
+
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({
+        token: token
     });
 });
 
