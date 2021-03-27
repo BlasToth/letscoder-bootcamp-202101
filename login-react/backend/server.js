8f476128327345791e573db0f95cc96b2fb4fcde
BlasToth <balazstothlaszlo+github@gmail.com>
2021-03-27 20:01:54 +0100

    build bcrypt to hash passwords

--- login-react/backend/server.js
+++ login-react/backend/server.js
@@ -2,10 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const users = [{
+    name: "Mike",
+    password: "mike"
+}];
 
 const posts = [
     {
@@ -18,19 +24,46 @@ const posts = [
     }
 ]
 
-app.use('/login', authenticateToken, (req, res) => {
-    const username = "Jon Nieve"
-    const user = { name: username };
+app.use('/login', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        const user = { name: req.body.username, password: hashedPassword }
+        users.push(user);
+        console.log(user)
+        return user
+    } catch {
+        res.status(500).send();
+    }
+const user2 = user;
     
-    const accToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    
+    const accToken = jwt.sign(user2, process.env.ACCESS_TOKEN_SECRET)
     res.json({
-        username,
+        username: user2.name,
         token: accToken
     });
 });
 
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.name === req.body.username)
+    if (user == null) {
+        return res.status(400).send("Cannot fond user")
+    }
+    try {
+       if (await bcrypt.compare(req.body.password, user.password)){
+           res.send("success")
+       } else {
+           res.send("Not allowed")
+       }
+    } catch {
+        res.status(500).send("")
+    }
+})
+
 app.get('/posts', (req, res) =>{
-    const username = "Jon Nieve"
+    const username = req.body.username;
+    console.log("POSTS: " + username)
     res.json(posts.filter(post => post.username === username))
 })
 
