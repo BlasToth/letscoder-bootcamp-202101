471ad4b5491beb065a0e887828e86ca5b0d0647f
BlasToth <balazstothlaszlo+github@gmail.com>
2021-03-27 11:35:19 +0100

    build logout functionality

--- login-react/login-react/src/App.js
+++ login-react/login-react/src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useState } from "react";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Login from "./components/Login/Login";
 import Preferences from "./components/Preferences/Preferences";
 import useToken from './useToken';
 
+function handleLogOut() {
+  localStorage.removeItem('token');   
+  <p>kshfghk</p> 
+}
+
+function alertLOggedOut() {
+  alert("Logged Out!");
+}
 
 function App() {
   const { token, setToken } = useToken();
@@ -16,6 +25,10 @@ function App() {
   return (
     <div className="wrapper">
       <h1>Application</h1>
+      <button onClick={() => {
+        handleLogOut();
+        alertLOggedOut();
+      }}>Log Out</button>
       <Router>
         <Switch>
           <Route path="/dashboard">
