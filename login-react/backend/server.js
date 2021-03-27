d9670540649d7f41b562a3a5cadcc740b6c8f6a3
BlasToth <balazstothlaszlo+github@gmail.com>
2021-03-27 18:07:57 +0100

    build autheticateToken()

--- login-react/backend/server.js
+++ login-react/backend/server.js
@@ -7,15 +7,38 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const posts = [
+    {
+        username: "Pepito Grillo",
+        title: 'Pepipost'
+    },
+    {
+        username: "Jon Nieve",
+        title: 'Jonipost'
+    }
+]
 
-app.use('/login', (req, res) => {
+
+app.use('/login', authenticateToken, (req, res) => {
     const username = "Pepito Grillo"
     const user = { name: username };
 
-    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    const accToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
     res.json({
-        token: token
+        token: accToken
     });
 });
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) return res.sendStatus(401).json({message: "No token"});
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403).json({message: "It is not a valid token - ACCESS DENIED"});
+        req.user = user;
+        next();
+    })
+}
 
 app.listen(4000, () => console.log('App listening on port 4000'))
