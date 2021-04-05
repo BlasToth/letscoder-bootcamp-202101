e5f4d1eefd3171f59661c900d0f20e6ddc5ec652
BlasToth <balazstothlaszlo+github@gmail.com>
2021-04-05 21:29:58 +0200

    refactor: Admindelete.js - connect form with the backend

--- proyecto-4-frontend/my-react-front/my-react-front/src/components/Admindelete.js
+++ proyecto-4-frontend/my-react-front/my-react-front/src/components/Admindelete.js
@@ -1,15 +1,37 @@
-// import './Nav.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 
 
 function Admindelete() {
+    const [id, setId] =useState();
+    console.log(id)
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const idOfTheVerbToBeDeleted = await submitDeleteDataForm({
+            id
+        });
+    }
+
+    async function submitDeleteDataForm(idOfTheVerbToBeDeleted) {
+        axios.delete("http://localhost:4000/deleteverb", {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: {
+              idOfTheVerbToBeDeleted
+            }
+          });
+    }
+
     return (
         <>
         <div className="delete-verb content">
         <h2>Delete a verb</h2>
-            <form action="/" method="DELETE">
-                <p>ID of the verb: <input type="text" name="sourceName" required/></p>
+            <form onSubmit={handleSubmit}>
+                <p>ID of the verb: <input type="text" name="id" required onChange={e => setId(e.target.value)} /></p>
                 <input type="submit" name="sub" value="DELETE" />
             </form>
         </div>
