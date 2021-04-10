cf2b4c0fa90a56aeaaaf51c993959c27c04fec0c
BlasToth <balazstothlaszlo+github@gmail.com>
2021-04-10 11:08:30 +0200

    build tooltip in Username.js

--- proyecto-4-frontend/my-react-front/my-react-front/src/components/userheader/Username.js
+++ proyecto-4-frontend/my-react-front/my-react-front/src/components/userheader/Username.js
@@ -1,19 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Button, Tooltip, OverlayTrigger } from "react-bootstrap";
 
 export default function Username(props) {
-    const admin = props.authAdminState.authAdminState;
-    const key = "🔑";
-    console.log(admin)
-    if (props.nickname.length) {
-        return (
-            <div>
-                Username: <span className="strong">{props.nickname} {admin && key} </span> 
-            </div>
-        )
-    }
+  const admin = props.authAdminState.authAdminState;
+  const key = "🔑";
+  console.log(admin);
+  if (props.nickname.length) {
     return (
+      <>
         <div>
-            Username: <span className="strong">Anonym </span> 
+          Username:{" "}
+          <span className="strong">
+            {props.nickname}
+            {["top"].map((placement) => (
+              <OverlayTrigger
+                key={placement}
+                placement={placement}
+                overlay={
+                  <Tooltip id={`tooltip-${placement}`}>
+                    You have <strong>admin</strong> privileges
+                  </Tooltip>
+                }
+              >
+                {admin && <span>{key}</span>}
+              </OverlayTrigger>
+            ))}
+          </span>
         </div>
-    )
+      </>
+    );
+  }
+  return (
+    <div>
+      Username: <span className="strong">Anonym </span>
+    </div>
+  );
 }
