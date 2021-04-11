d5aa78801087e95fc491049f4e661f12aeeabc13
BlasToth <balazstothlaszlo+github@gmail.com>
2021-04-11 13:34:50 +0200

    add validations to back

---
+++ proyecto-3-own-project/validations/validations.js
@@ -0,0 +1,13 @@
+const validations = {
+    validateEmail(email){
+        if(typeof email !== 'string') throw new TypeError(`${email} is not an e-mail`)
+        if(!email.trim().length) throw new Error ('Email cannot be an empty string')
+    },
+    validatePassword(password){
+        if(typeof password !== 'string') throw new TypeError(`${password} is not a password`)
+        if(!password.trim().length) throw new Error('The password cannot be empty')
+        if(password.length < 6 ) throw new Error ("Password's length must be greater than 6")
+    }
+}
+
+module.exports = validations
