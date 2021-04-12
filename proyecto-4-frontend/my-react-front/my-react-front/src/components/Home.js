ae1a957e31ea1f5dd4cffaa365839a1c1c70a9c9
BlasToth <balazstothlaszlo+github@gmail.com>
2021-04-12 22:53:27 +0200

    add sharks and content to home page

--- proyecto-4-frontend/my-react-front/my-react-front/src/components/Home.js
+++ proyecto-4-frontend/my-react-front/my-react-front/src/components/Home.js
@@ -3,6 +3,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Login from './Login';
 import Signup from "./Signup";
 import { useState } from 'react';
+import Sharky from "../images/tiburon_refran.jpg";
+import { Container } from "react-bootstrap";
+
 
 const token = localStorage.getItem('token');
 function Home() {
@@ -21,10 +24,13 @@ function Home() {
       );
     }
     return(
-      <>
+      <Container>
       <div className="title">Home</div>
       <div className="home-text">Si das pescado a un hombre hambriento, le nutres una jornada. Si le enseñas a pescar, le nutrirás toda la vida </div>
-      </>
+      <div className="home-image">
+        <img className="shark-commonphrase" src={Sharky}  alt="shark saying"></img>
+      </div>
+      </Container>
     )
 }
 
